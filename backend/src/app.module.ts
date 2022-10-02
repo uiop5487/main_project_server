d5ea6eedf04db6c9d3c4e87d3072f2b220cdc7b5
uiop5487 <uiop5487@gmail.com>
2022-10-02 23:31:37 +0900

    [ADD] controller API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,7 @@ import * as redisStore from 'cache-manager-redis-store';
 import { ProductReviewsModule } from './apis/productsReview/productReviews.module';
 import { ProductInquiriesModule } from './apis/productsInquiry/productInquiries.module';
 import { ProductInquiryAnswersModule } from './apis/productsInquiryAnswers/productInquiryAnswers.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -51,5 +52,6 @@ import { ProductInquiryAnswersModule } from './apis/productsInquiryAnswers/produ
       isGlobal: true,
     }),
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
